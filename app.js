71f66fc044eea49936681d76bde591764a07a528
www-data@vpn1.onztv.com <admin@onztv.com>
2016-01-15 01:43:34 -0800

    simple

--- app.js
+++ app.js
@@ -1,45 +1,25 @@
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs")
-    port = process.argv[2] || 8888;
+var http = require('http')
+var url = require('url')
+var fs = require('fs')
+var baseDirectory = __dirname // or whatever base directory you want
 
-http.createServer(function(request, response) {
+http.createServer(function (request, response) {
+   var requestUrl = url.parse(request.url)
+   var fsPath = baseDirectory+requestUrl.pathname
 
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
+   console.log(requestUrl);
 
-  console.log(uri);
-  response.writeHead(200);
-  response.write('loaderio-0674f7d118faaea5c52856de4f1d99ff');
-  response.end();
-
-  /*
-  path.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
-
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
-
-      response.writeHead(200);
-      response.write(file, "binary");
-      response.end();
-    });
-  });
-  */
-
-}).listen(parseInt(port, 10));
-
-console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+   fs.exists(fsPath, function(exists) {
+     try {
+       if(exists) {
+         response.writeHead(200)
+	response.write('loaderio-0674f7d118faaea5c52856de4f1d99ff');
+//         fs.createReadStream(fsPath).pipe(response) // do NOT use fs's sync methods (e.g readFileSync) ANYWHERE on production
+       } else {
+         response.writeHead(500)
+       }
+     } finally {
+        response.end() // inside finally so errors don't make browsers hang
+     } 
+   })
+}).listen(2000)
